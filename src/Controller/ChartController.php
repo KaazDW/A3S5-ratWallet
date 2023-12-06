0a4ce5ef54baa4cdf4aca9b8171b97bd2409b07d
TRONTIN Pauline <morgane71480@gmail.com>
2023-12-06 20:28:50 +0100

    [dev] Add Pie Chart test

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -12,17 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class ChartController extends AbstractController
 {
     #[Route('/chart-line/{id}', name: 'chart_line')]
-    public function chart(int $id,EntityManagerInterface $entityManager): Response
+    public function chart(int $id, EntityManagerInterface $entityManager): Response
     {
+        $historyDataResponse = $this->forward('App\Controller\ChartController::chartData', [
+            'id' => $id,
+            'entityManager' => $entityManager,
+        ]);
+
+        $data = json_decode($historyDataResponse->getContent(), true);
+
         $account = $entityManager->getRepository(Account::class)->find($id);
 
-        return $this->render('pages/chartLine.html.twig',[
+        return $this->render('pages/chartLine.html.twig', [
             'account' => $account,
-            ]);
+            'data' => $data,
+        ]);
     }
 
     #[Route('/chart-data/{id}', name: 'chart_data')]
-    public function chartData(int $id, EntityManagerInterface $entityManager,): Response
+    public function chartData(int $id, EntityManagerInterface $entityManager): Response
     {
         $historyData = $entityManager->getRepository(History::class)->findBy(['account' => $id]);
 
@@ -45,4 +53,20 @@ class ChartController extends AbstractController
         return $this->json($data);
     }
 
+    /**
+     * @Route("/pie-chart", name="pie_chart")
+     */
+    #[Route('/pie-chart-category', name: 'pie_chart_category')]
+    public function pieChartCategory(EntityManagerInterface $entityManager): Response
+    {
+        $categories = $entityManager->getRepository('App\Entity\Category')->findAll();
+
+        $data = [];
+        foreach ($categories as $category) {
+            $data['labels'][] = $category->getLabel();
+            $data['data'][] = $category->getLabel();
+        }
+
+        return $this->json($data);
+    }
 }
