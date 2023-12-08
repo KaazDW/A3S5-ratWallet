8881ebb311186b4e0c36f0ccb161a3bbbd8041c2
TRONTIN Pauline <morgane71480@gmail.com>
2023-12-08 13:42:21 +0100

    [dev] Add Goal progress chart

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Entity\Goal;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChartController extends AbstractController
@@ -20,4 +24,31 @@ class ChartController extends AbstractController
         return new JsonResponse($chartData);
         // for see which data are responses go to : http://127.0.0.1:8000/chart/data
     }
+
+    #[Route('/goal-progress/{id}', name: 'goal_progress')]
+    public function goalProgress(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $account = $entityManager->getRepository(Account::class)->find($id);
+
+        // Assuming getGoal() returns a single goal
+        $goal = $account->getGoal();
+
+        // Access the targetAmount directly
+        $totalGoalAmount = $goal ? $goal->getTargetAmount() : 0;
+
+        dump($totalGoalAmount);
+
+        if ($totalGoalAmount === 0) {
+            $progress = 0;
+        } else {
+            $progress = ($account->getBalance() / $totalGoalAmount) * 100;
+        }
+
+        dump($progress);
+
+        return $this->render('pages/test.html.twig', [
+            'progress' => $progress,
+        ]);
+    }
+
 }
